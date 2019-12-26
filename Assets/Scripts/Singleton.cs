69b65911117f5c152342e512a0d2fa34eefe5db1
shogo ogawa <kuro.1203.kuro@gmail.com>
2019-12-27 01:34:04 +0900

    ad::シングルトンクラス

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,19 @@
+﻿
+/// <summary>
+/// シングルトンの基底クラス
+/// </summary>
+/// <typeparam name="T">シングルトンにしたい派生クラス</typeparam>
+public class Singleton<T> where T : class, new()
+{
+    private static T instance = null;
+
+    public static T GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new T();
+        }
+
+        return instance;
+    }
+}
